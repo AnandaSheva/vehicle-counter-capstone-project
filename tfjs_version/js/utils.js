93bfefedc6bfb032ac649b51734017dc35d7bda5
Atma1 <atmasusanto08@gmail.com>
2024-11-30 14:57:47 +0800

    feat: moved some function to util

--- tfjs_version/js/utils.js
+++ tfjs_version/js/utils.js
@@ -6,3 +6,18 @@ const xywhToxyxy = (xywh) => {
     return [parseInt(x1), parseInt(y1), parseInt(x2), parseInt(y2)]
 };
 
+function triggerLine(x, y, w, h) {
+    const [x1, y1, x2, y2] = xywhToxyxy(x, y, w, h);
+    console.log(x1, y1, x2, y2);
+
+    const bboxYCenter = Math.floor((y1 + y2) / 2)
+    const lineYCenter = Math.floor((linePoint1[1] + linePoint2[1]) / 2)
+    return lineYCenter < (bboxYCenter + offset) && lineYCenter > (bboxYCenter - offset);
+}
+
+function updateCounter(cls, counter) {
+    if (cls in counter) {
+        counter[cls]++;
+    }
+}
+
